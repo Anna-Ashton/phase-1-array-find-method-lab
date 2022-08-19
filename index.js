5ff26d2626faff64a9436441f6ec52d9a5412060
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-08-19 06:05:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+let superbowlWin = winYearDated => {
+	let winYear = winYearDated.find( bowl => {  return bowl.result === "W" })
+	if (winYear){ 
+		return winYear.year
+	} else {
+		return undefined
+	}
+}
